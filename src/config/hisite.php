1d2b0b6a5637697c04e1b73a7d848787d94984ad
tofid <andreyklochok@gmail.com>
2016-10-20 12:51:58 +0300

    Added hisite config

---
+++ src/config/hisite.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'components' => [
+        'themeManager' => [
+            'defaultTheme' => 'adminlte',
+            'assets' => [
+                \rutld\assets\rutld\Asset::class,
+            ],
+        ],
+    ]
+];
