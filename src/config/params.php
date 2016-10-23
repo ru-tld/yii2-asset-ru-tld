6a811ce788832d8c44a7c1ef33ef06421e118143
Andrii Vasyliev <sol@hiqdev.com>
2016-10-23 16:29:55 +0000

    redone social links

--- src/config/params.php
+++ src/config/params.php
@@ -9,16 +9,21 @@
  * @copyright Copyright (c) 2015, RU-TLD (https://ru-tld.ru/)
  */
 
+$supportEmail = 'support@ru-tld.ru';
+
 return [
     'organizationName'  => 'RU-TLD',
     'organizationUrl'   => 'https://ru-tld.ru/',
-    'supportEmail'      => 'support@ru-tld.ru',
-    'adminEmail'        => 'support@ru-tld.ru',
+    'supportEmail'      => $supportEmail,
+    'adminEmail'        => $supportEmail,
     'abuseEmail'        => 'abuse@ru-tld.ru',
     'salesEmail'        => 'sale@ru-tld.ru',
     'logoImage'         => '@rutld/assets/rutld/assets/img/alt-logo.png',
     'logoOptions'       => ['style' => 'width: 100px;'],
 
-    'twitter_link'      => 'https://twitter.com/rutld',
-    'facebook_link'     => 'https://www.facebook.com/groups/230254030432488/',
+    'socialLinks'       => [
+        'email'     => 'mailto:' . $supportEmail,
+        'twitter'   => 'https://twitter.com/rutld',
+        'facebook'  => 'https://www.facebook.com/groups/230254030432488/',
+    ],
 ];
